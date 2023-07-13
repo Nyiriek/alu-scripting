ae416121e56e4e1848d941a296f128e62d8044d5
Nyiriek <n.peat@alustudent.com>
2023-07-13 18:46:04 +0000

    Top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,10 +8,10 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
     res = requests.get(url, headers=headers)
-    
+
     if res.status != 200:
         print(none)
     else:
         data = res.json().get('data').get('children')
         for post in data:
-            print(post.get('data').get('title')) 
+            print(post.get('data').get('title'))
