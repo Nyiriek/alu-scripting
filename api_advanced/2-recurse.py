1ef885d3f21777c82d28593b3b1fe185780b9ece
Nyiriek <n.peat@alustudent.com>
2023-07-16 10:27:56 +0000

    Recurse it!

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'My Agent 1.0'}
+    params = {'after': after}
+    res = requests.get(url, headers=headers, params=params)
+    
+    if res.status == 200:
+        json_res = res.json()
+        after = json_res.get('data').get('after')
+        has_next = json_res.get('data').get('data') is not None
+        
+        hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title'))
+         for article in hot_articles]
+        
+        return recurse(subreddit, hot_list, after=after) if has_next \
+            else hot_list
+        
+    else:
+        return None
